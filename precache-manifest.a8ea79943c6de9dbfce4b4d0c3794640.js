9246db0897d48f25e6c61d33751846ac872b4c13
neven4 <35399376+neven4@users.noreply.github.com>
2018-12-08 23:20:28 +0300

    Updates

---
+++ precache-manifest.a8ea79943c6de9dbfce4b4d0c3794640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70454419431d82861a64",
+    "url": "/testCurr/static/css/main.e8603fc5.chunk.css"
+  },
+  {
+    "revision": "70454419431d82861a64",
+    "url": "/testCurr/static/js/main.70454419.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/testCurr/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "a37dfc865bb53744e427",
+    "url": "/testCurr/static/js/runtime~main.a37dfc86.js"
+  },
+  {
+    "revision": "3c62ae97fa50697d5d8dee6d6793ed28",
+    "url": "/testCurr/index.html"
+  }
+];
