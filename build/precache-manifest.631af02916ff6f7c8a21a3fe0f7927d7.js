d5a8fbfb3b32c38b459eee03268913bc6c3aceca
neven4 <35399376+neven4@users.noreply.github.com>
2018-12-08 22:55:23 +0300

    build pack

---
+++ build/precache-manifest.631af02916ff6f7c8a21a3fe0f7927d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f792a2824316f6afed1",
+    "url": "/static/css/main.e8603fc5.chunk.css"
+  },
+  {
+    "revision": "4f792a2824316f6afed1",
+    "url": "/static/js/main.4f792a28.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8f8ba032db02dc0cc04def661ee77e0",
+    "url": "/index.html"
+  }
+];
